1a7301d0618b738e16ceff97e1bb297d2fc07476
vishaljagtap3 <vishal.jagtap@bitcode.in>
2021-02-18 14:50:38 +0530

    Fragments2

--- app/src/main/java/in/bitcode/fragments2/LoginFragment.java
+++ app/src/main/java/in/bitcode/fragments2/LoginFragment.java
@@ -16,6 +16,17 @@ public class LoginFragment extends Fragment {
     private EditText mEdtUsername, mEdtPassword;
     private Button mBtnLogin;
 
+    public interface OnLoginListener {
+        public void onSuccess(LoginFragment loginFragment, String username);
+        public void onFailure(LoginFragment loginFragment, String username);
+    }
+
+    private OnLoginListener mOnLoginListener;
+
+    public void setOnLoginListener(OnLoginListener onLoginListener) {
+        this.mOnLoginListener = onLoginListener;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -33,6 +44,12 @@ public class LoginFragment extends Fragment {
     private class BtnLoginClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
+
+            if( mOnLoginListener == null) {
+                return;
+            }
+
+
             if(mEdtUsername.getText().toString().equals("bitcode") && mEdtPassword.getText().toString().equals("bitcode")) {
                 //way 1
 
@@ -43,17 +60,31 @@ public class LoginFragment extends Fragment {
                         .commit();
                  */
 
+                /*
                 HomeFragment homeFragment = new HomeFragment();
 
                 Bundle input = new Bundle();
                 input.putString("username", mEdtUsername.getText().toString());
                 homeFragment.setArguments(input);
 
-                getActivity().getSupportFragmentManager()
+                //getActivity().getSupportFragmentManager()
+                getFragmentManager()
                         .beginTransaction()
                         .add(R.id.mainContainer, homeFragment, null)
                         .addToBackStack(null)
                         .commit();
+                        */
+                 //way 2
+                mOnLoginListener.onSuccess(
+                        LoginFragment.this,
+                        mEdtUsername.getText().toString()
+                );
+            }
+            else {
+                mOnLoginListener.onFailure(
+                        LoginFragment.this,
+                        mEdtUsername.getText().toString()
+                );
             }
         }
     }
--- app/src/main/java/in/bitcode/fragments2/MainActivity.java
+++ app/src/main/java/in/bitcode/fragments2/MainActivity.java
@@ -6,9 +6,42 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LoginFragment mLoginFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLoginFragment =
+                (LoginFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.loginFragment);
+
+        mLoginFragment.setOnLoginListener( new LoginFragmentListener() );
+    }
+
+    private class LoginFragmentListener implements LoginFragment.OnLoginListener{
+
+        @Override
+        public void onSuccess(LoginFragment loginFragment, String username) {
+
+            HomeFragment homeFragment = new HomeFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString("username", username);
+            homeFragment.setArguments(bundle);
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.mainContainer, homeFragment, null)
+                    .addToBackStack(null)
+                    .commit();
+
+            //getSupportFragmentManager().popBackStack();
+        }
+
+        @Override
+        public void onFailure(LoginFragment loginFragment, String username) {
+
+        }
     }
 }
